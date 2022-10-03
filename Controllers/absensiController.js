68b34e647ed44c18681a5ac0401e4cff1fb52140
ranuaa <white_angleable@yahoo.com>
2022-10-03 02:11:37 -0700

    add absensi fix

--- Controllers/absensiController.js
+++ Controllers/absensiController.js
@@ -1,32 +1,79 @@
 import absensiModel from "../models/absensiModel.js";
 import moment from 'moment'
 
+
+//get all data
+export const getAllAbsensi = async(req,res) => {
+    try {
+        const data = await absensiModel.find()
+        res.status(200).json({
+            data: data
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        })
+    }
+}
+
+//get a data 
+export const getSingleAbsensi = async(req,res) =>{
+    const userId = req.params.id
+    try {
+        const data = await absensiModel.find({userId})
+        res.status(200).json({
+            data: data
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        })
+    }
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
 //add new data
 export const addAbsensi = async(req,res) => {
-    const {userId} = req.body
-    // const today = moment().format("DD-MM-YYYY")
     const newData = new absensiModel(req.body)
-    var today = new Date();
-    var dd = String(today.getDate()).padStart(2, '0');
-    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-    var yyyy = today.getFullYear();
-
-    today = mm + '/' + dd + '/' + yyyy;
-    const findData = await absensiModel.find({userId : userId, date: today})
     try {
-        if(!findData){
-            const data = await newData.save()
-            res.status(200).json({
-                message: "success",
-                data: data
-            })
-        }else{
-            res.json({
-                message: "failed",
-                data: findData,
-            })
-            
-        }
+        const data = await newData.save()
+        res.status(200).json({
+            data: data
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        })
+    }
+}
+
+//edit data
+export const editAbsensi = async(req,res) => {
+    const id = req.params.id
+    const {_id, userId, ...details} = req.body
+    try {
+        const updateUser = await absensiModel.findByIdAndUpdate(id, details, {new : true})
+        res.status(200).json({
+            data: updateUser
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        })
+    }
+}
+
+export const deleteAbsensi = async(req,res) => {
+    const id = req.params.id
+    try {
+        const data = await absensiModel.findByIdAndDelete(id)
+        res.status(200).json({
+            message: "success delete data"
+        })
     } catch (error) {
         res.status(500).json({
             message: error.message
